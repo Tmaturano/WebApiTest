f52f308f0f5548d78440728bbb7df1c53a77e0d8
Thiago Maturana <thiagomatu@hotmail.com>
2016-01-07 18:59:45 -0200

    First Checkin

---
+++ ConsumidorDeServicos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using WebApiTeste.Models;
+
+namespace ConsumidorDeServicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://localhost:57142/api/");
+
+            var request = new RestRequest("Restaurante/FindAll", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            
+            //Assíncrono
+            client.ExecuteAsync(request, response => {
+                                    Console.WriteLine("Serviço BuscarTodosRestaurantes()");
+                                    Console.WriteLine(response.Content); 
+                                });
+
+            //Síncrono
+            //IRestResponse responseSincrono = client.Execute(request);
+           
+            var request2 = new RestRequest("Restaurante/BuscarRestaurantePorId", Method.GET);
+            request2.AddParameter("id", 1);
+            request2.RequestFormat = DataFormat.Json;
+
+            //Assíncrono
+            client.ExecuteAsync<Restaurante>(request2, response => {
+                                Console.WriteLine();
+                                Console.WriteLine("Serviço BuscarRestaurantePorId()");
+                                Console.WriteLine(response.Data.Id);
+                                Console.WriteLine(response.Data.Nome);
+                                Console.WriteLine(response.Data.Endereco);
+                            });
+
+            //Síncrono
+            //IRestResponse<Restaurante> response2 = client.Execute<Restaurante>(request2);
+            //var teste = response2.Data.Nome;
+
+                                                
+            Restaurante rest = new Restaurante();
+            rest.Id = 1;
+            rest.Nome = "Restaurante Ziquinha";
+            rest.Endereco = "Avenida Rio Branco";                       
+
+            var request3 = new RestRequest("Restaurante/AdicionarRestaurante", Method.POST);
+
+            var json = request3.JsonSerializer.Serialize(rest);                        
+            request.RequestFormat = DataFormat.Json;
+            request3.AddParameter("restaurante", json);            
+            
+            client.ExecuteAsync<Restaurante>(request3, response =>
+                                            {
+                                                Console.WriteLine();
+                                                Console.WriteLine("Serviço AdicionarRestaurante()");
+                                                Console.WriteLine(response.StatusDescription);
+                                            });
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApiTeste/Controllers/RestauranteController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApiTeste.Models;
+
+namespace WebApiTeste.Controllers
+{
+    public class RestauranteController : ApiController
+    {
+        private List<Restaurante> _restaurantes = null;
+
+        public RestauranteController()
+        {
+            if (_restaurantes == null)
+            {
+                _restaurantes = new List<Restaurante>();
+                _restaurantes.Add(new Restaurante() { Id = 1, Nome = "Restaurante Linx Joinville", Endereco = "Rua Tenente Antônio João" });
+                _restaurantes.Add(new Restaurante() { Id = 2, Nome = "Restaurante Linx São Paulo", Endereco = "Rua Morumbi" });
+                _restaurantes.Add(new Restaurante() { Id = 3, Nome = "Restaurante Linx Rio de Janeiro", Endereco = "Rua Copacabana"});
+            }
+        }
+
+        [HttpGet]
+        [ActionName("FindAll")]        
+        public IEnumerable<Restaurante> BuscarTodosRestaurantes()
+        {
+            return _restaurantes;
+        }
+
+                        
+        [AcceptVerbs("GET")]
+        public IHttpActionResult BuscarRestaurantePorId(int id)
+        {
+            var restaurante = _restaurantes.FirstOrDefault(p => p.Id.Equals(id));
+            
+            if (restaurante == null)
+                return NotFound();
+
+            return Ok(restaurante);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage AdicionarRestaurante(Restaurante restaurante)
+        {
+            if (!CheckIfRestaurantExists(restaurante.Id))
+            {
+                _restaurantes.Add(restaurante);
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+
+        private bool CheckIfRestaurantExists(int id)
+        {
+            return( _restaurantes.Where(p => p.Id.Equals(id)).Any());            
+        }
+
+    }
+}
